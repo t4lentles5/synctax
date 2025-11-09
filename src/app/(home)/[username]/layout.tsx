e8576731b0a92de11703f26287d036042d730309
Omar Obregon <oobregon544@gmail.com>
2025-11-09 18:50:55 -0500

    feat: Enhance user profile page responsiveness and styling
    This commit introduces significant improvements to the user profile page, focusing on responsive design and updated styling.
    
    User Profile Layout:
    - Implemented responsive adjustments for avatar size, full name, and bio display across different screen sizes (`sm`, `md`, `lg`).
    - Added conditional rendering for "Edit Profile" or "Follow/Message" buttons based on whether the authenticated user is viewing their own profile.
    - Adjusted the layout of social links and user statistics for better responsiveness.
    
    Global Styles:
    - Introduced new CSS variables `--color-background-secondary` and `--color-background-secondary-hover` for both light and dark themes to support new UI elements, particularly the action buttons on the user profile.

--- src/app/(home)/[username]/layout.tsx
+++ src/app/(home)/[username]/layout.tsx
@@ -49,46 +49,60 @@ export default async function UserLayout({
     userByUsername.username === authenticatedUser.username;
 
   return (
-    <div className='flex w-full max-w-4xl flex-col gap-10 pt-10'>
-      <main className='flex w-full gap-10'>
+    <div className='flex w-full max-w-3xl flex-col gap-5 p-10 lg:max-w-4xl'>
+      <main className='flex w-full gap-5 sm:gap-10'>
         <img
           src={userByUsername.avatar_url}
           alt=''
-          className='size-40 rounded-full'
+          className='size-24 rounded-full sm:size-40'
         />
         <div className='flex flex-col gap-5'>
-          <div className='flex gap-20'>
-            <div className='flex flex-col gap-1'>
-              <h3 className='text-3xl font-semibold'>
+          <div className='flex flex-col gap-1'>
+            <div className='flex items-center gap-10 lg:gap-20'>
+              <h3 className='text-2xl font-semibold lg:text-3xl'>
                 {userByUsername.fullname}
               </h3>
+
+              <div className='hidden md:block'>
+                {isAuthenticatedUser ? (
+                  <Link
+                    href='/settings'
+                    className='hover:bg-background-secondary-hover bg-background-secondary ml-5 rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-300 ease-in-out'
+                  >
+                    Edit Profile
+                  </Link>
+                ) : (
+                  <>
+                    <button className='hover:bg-background-secondary-hover bg-background-secondary cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-300 ease-in-out'>
+                      Follow
+                    </button>
+                    <button className='hover:bg-primary-deep bg-primary ml-5 cursor-pointer rounded-lg px-4 py-2 text-sm font-semibold transition-colors duration-300 ease-in-out'>
+                      Message
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-1'>
               <h4 className='text-foreground-muted'>
                 @{userByUsername.username}
               </h4>
-              <p>{userByUsername.bio}</p>
-            </div>
-
-            <div className=''>
-              <button className='hover:border-primary border-background cursor-pointer rounded-lg border px-3 py-2 transition-colors duration-300 ease-in-out'>
-                Follow
-              </button>
-              <button className='hover:bg-primary-deep bg-primary ml-5 cursor-pointer rounded-lg px-3 py-2 transition-colors duration-300 ease-in-out'>
-                Message
-              </button>
+              <p className='hidden text-sm sm:block'>{userByUsername.bio}</p>
             </div>
           </div>
 
-          <div className='flex gap-3'>
-            <p className='flex flex-col gap-2 text-sm md:flex-row'>
+          <div className='hidden gap-5 sm:flex'>
+            <p className='flex gap-2 text-sm'>
               <span className='text-primary font-medium'>42</span>{' '}
               <span className='text-foreground-muted'>Projects</span>
             </p>
 
-            <button className='flex flex-col gap-2 text-sm md:flex-row'>
+            <button className='flex gap-2 text-sm'>
               <span className='text-primary font-medium'>1.2k</span>{' '}
               <span className='text-foreground-muted'>Followers</span>
             </button>
-            <button className='flex flex-col gap-2 text-sm md:flex-row'>
+            <button className='flex gap-2 text-sm'>
               <span className='text-primary font-medium'>1.2k</span>{' '}
               <span className='text-foreground-muted'>Following</span>
             </button>
@@ -96,7 +110,25 @@ export default async function UserLayout({
         </div>
       </main>
 
-      <section className='flex gap-5'>
+      <p className='block text-sm sm:hidden'>{userByUsername.bio}</p>
+
+      <div className='border-border flex justify-between gap-3 border-y p-3 sm:hidden'>
+        <button className='flex flex-col gap-2 text-sm'>
+          <span className='text-primary font-medium'>42</span>{' '}
+          <span className='text-foreground-muted'>Projects</span>
+        </button>
+
+        <button className='flex flex-col gap-2 text-sm'>
+          <span className='text-primary font-medium'>1.2k</span>{' '}
+          <span className='text-foreground-muted'>Followers</span>
+        </button>
+        <button className='flex flex-col gap-2 text-sm'>
+          <span className='text-primary font-medium'>1.2k</span>{' '}
+          <span className='text-foreground-muted'>Following</span>
+        </button>
+      </div>
+
+      <section className='flex justify-center gap-5 sm:justify-start'>
         <Link href=''>
           <span
             className='icon-[mage--github] hover:text-primary text-foreground-muted size-6 transition-colors duration-300 ease-in-out'
@@ -121,6 +153,26 @@ export default async function UserLayout({
           />
         </Link>
       </section>
+
+      <div className='flex gap-5 md:hidden'>
+        {isAuthenticatedUser ? (
+          <Link
+            href='/settings'
+            className='hover:bg-background-secondary-hover bg-background-secondary w-full rounded-lg py-2 text-center text-sm font-semibold transition-colors duration-300 ease-in-out'
+          >
+            Edit Profile
+          </Link>
+        ) : (
+          <>
+            <button className='hover:bg-background-secondary-hover bg-background-secondary w-full cursor-pointer rounded-lg py-2 text-sm font-semibold transition-colors duration-300 ease-in-out'>
+              Follow
+            </button>
+            <button className='hover:bg-primary-deep bg-primary w-full cursor-pointer rounded-lg py-2 text-sm font-semibold transition-colors duration-300 ease-in-out'>
+              Message
+            </button>
+          </>
+        )}
+      </div>
 
       {children}
     </div>
